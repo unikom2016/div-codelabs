428be500948e5a9da2c8a0887fc9a2820e1269f7
Mochamad Iqbal Dwi Cahyo <mochadwi@users.noreply.github.com>
2017-08-01 14:09:12 +0700

    [Add] Ways to get DOM

--- phase_4/workshop/2017/wk01/day03/assets/js/event.js
+++ phase_4/workshop/2017/wk01/day03/assets/js/event.js
@@ -1,4 +1,14 @@
+window.onload = function() {
 // query -> array
-document.querySelectorAll("button")[0].onclick = function() {
-    window.alert("Clicked!");
+// document.querySelectorAll("button")[0].onclick = function() {
+//     window.alert("Clicked!");
+// }
+
+// function customAlert() {
+//     window.alert("Clicked");
+// }
+
+    document.querySelectorAll("button")[0].addEventListener('click', function() {
+        window.alert("Clicked!");
+    })
 }
