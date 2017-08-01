c3d59b4a43c44cfde8606975174336dadc3d69dc
Mochamad Iqbal Dwi Cahyo <mochadwi@users.noreply.github.com>
2017-08-01 14:33:08 +0700

    [Add] jQuery CDN

--- phase_4/workshop/2017/wk01/day03/assets/js/event.js
+++ phase_4/workshop/2017/wk01/day03/assets/js/event.js
@@ -1,14 +1,18 @@
-window.onload = function() {
-// query -> array
-// document.querySelectorAll("button")[0].onclick = function() {
-//     window.alert("Clicked!");
+// window.onload = function() {
+    // query -> array
+    // document.querySelectorAll("button")[0].onclick = function() {
+    //     window.alert("Clicked!");
+    // }
+
+    // function customAlert() {
+    //     window.alert("Clicked");
+    // }
+
+    // document.querySelectorAll("button")[0].addEventListener('click', function() {
+    //     window.alert("Clicked!");
+    // })
 // }
 
-// function customAlert() {
-//     window.alert("Clicked");
-// }
-
-    document.querySelectorAll("button")[0].addEventListener('click', function() {
-        window.alert("Clicked!");
-    })
-}
+$("button").click(function() {
+    window.alert("hello world!");
+});
